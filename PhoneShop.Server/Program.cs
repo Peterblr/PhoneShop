ec6c1e7cde4272c4885a24187384a08733390e25
PSviryd <piotr.sviryd@symfa.com>
2024-03-22 12:01:48 +0300

    added NuGet package and configured lounchSettings files

--- PhoneShop.Server/Program.cs
+++ PhoneShop.Server/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,12 +15,17 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
-
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
