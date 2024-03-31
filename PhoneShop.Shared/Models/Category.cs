c14378f7ec4a54addd1bce77ead015d6454b45fd
PSviryd <piotr.sviryd@symfa.com>
2024-03-31 22:00:18 +0300

    add relationsheeps for Product and Category models

---
+++ PhoneShop.Shared/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PhoneShop.Shared.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //Relationship : One to Many
+        public List<Product>? Products { get; set; }
+    }
+}
