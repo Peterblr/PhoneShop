9ad79a72d612f64679eb4604dde159a7f9143bba
PSviryd <piotr.sviryd@symfa.com>
2024-04-07 20:27:28 +0300

    add product controller

---
+++ PhoneShop.Server/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneShop.Server.Repositories;
+using PhoneShop.Shared.Models;
+using PhoneShop.Shared.Services;
+
+namespace PhoneShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController(ICategory categoryService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetAllCategories()
+        {
+            var categories = await categoryService.GetAllCategories();    
+
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse>> AddCategory(Category category)
+        {
+            if (category == null) return BadRequest("Category is null");
+
+            var response = await categoryService.AddCategory(category);
+
+            return Ok(response);
+        }
+    }
+}
